9b69818507d24d704dfb4483b15ed5b7813c2b0a
Masato Ikeda <masato.ikeda@gmail.com>
2019-05-27 01:00:12 +0900

    Refactored 3

--- 3/main.c
+++ 3/main.c
@@ -1,52 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-typedef struct {
-  long *data;
-  int capacity;
-  int len;
-} Vector;
-
-Vector *vec_new(void) {
-  Vector *v = malloc(sizeof(Vector));
-  v->len = 0;
-  v->capacity = 16;
-  v->data = malloc(sizeof(long) * v->capacity);
-  return v;
-}
-
-void vec_free(Vector *v) {
-  free(v->data);
-  free(v);
-}
-
-void vec_push(Vector *v, long elm) {
-  if (v->len >= v->capacity) {
-    v->capacity *= 2;
-    v->data = realloc(v->data, sizeof(long) * v->capacity);
-  }
-  v->data[v->len++] = elm;
-}
 
 long solve(long target) {
-  Vector *factors = vec_new();
+  long max = 0;
 
   long x = target;
   long i = 2;
   while (x > 1) {
     if (x % i == 0) {
-      vec_push(factors, i);
+      max = i;
       x = x / i;
     } else {
       i++;
     }
   }
 
-  if (factors->len == 0) {
-    return 0;
-  } else {
-    return factors->data[factors->len - 1];
-  }
+  return max;
 }
 
 int main(int argc, char **args) {
