6bb95a9e65ede87e28e07e03e9d36f040b9e31cb
Masato Ikeda <masato.ikeda@gmail.com>
2019-05-27 00:52:02 +0900

    Retried 3, but failed

--- 3/main.c
+++ 3/main.c
@@ -1,134 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 typedef struct {
-  long *data;
-  int capacity;
+  long *values;
   int len;
-} Vector;
+} Primes;
 
-Vector *vec_new(void) {
-  Vector *v = malloc(sizeof(Vector));
-  v->len = 0;
-  v->capacity = 16;
-  v->data = malloc(sizeof(long) * v->capacity);
-  return v;
-}
-
-void vec_free(Vector *v) {
-  free(v->data);
-  free(v);
-}
-
-void vec_push(Vector *v, long elm) {
-  if (v->len >= v->capacity) {
-    v->capacity *= 2;
-    v->data = realloc(v->data, sizeof(long) * v->capacity);
+void primes_dump(Primes *primes) {
+  for (int i = 0; i < primes->len; i++) {
+    printf("  [%d] = %ld\n", i, primes->values[i]);
   }
-  v->data[v->len++] = elm;
 }
 
-long vec_length(Vector *v) {
-  return v->len;
-}
-
-long vec_last(Vector *v) {
-  return v->data[v->len - 1];
-}
-
-long vec_get(Vector *v, int i) {
-  return v->data[i];
-}
-
-typedef struct {
-  Vector *primes;
-} PG;
-
-PG *pg_new(void) {
-  PG *pg = malloc(sizeof(PG));
-  pg->primes = vec_new();
-  return pg;
-}
-
-void pg_free(PG *pg) {
-  vec_free(pg->primes);
-  free(pg);
-}
-
-int pg_is_prime(PG *pg, long try) {
-  for (int i = 0; i < vec_length(pg->primes); i++) {
-    long p = vec_get(pg->primes, i);
-    if (try % p == 0) {
-      return 0;
-    }
+void primes_prepare(Primes *primes, long target) {
+  long limit = ceil(sqrt(target));
+  long *flags = malloc((target + 1) * sizeof(long));
+  for (long i = 2; i <= target; i++) {
+    flags[i] = 1;
   }
-
-  return 1;
-}
-
-long pg_next(PG *pg) {
-  if (vec_length(pg->primes) == 0) {
-    vec_push(pg->primes, (long)2);
-  } else if (vec_length(pg->primes) == 1) {
-    vec_push(pg->primes, (long)3);
-  } else {
-    long try = vec_last(pg->primes);
-    while (1) {
-      try += (long)2;
-      if (pg_is_prime(pg, try)) {
-        vec_push(pg->primes, try);
-        break;
+  for (long i = 2; i <= limit; i++) {
+    if (flags[i]) {
+      for (long j = 2; i * j <= target; j++) {
+        flags[i * j] = 0;
       }
     }
   }
-  return vec_last(pg->primes);
-}
-
-long resolve(long target) {
-  Vector *factors = vec_new();
-
-  long p;
-  PG *pg = pg_new();
-  while ((p = pg_next(pg)) <= target) {
-    if (target % p == 0) {
-      vec_push(factors, p);
+  for (long i = 2; i <= target; i++) {
+    if (flags[i]) {
+      primes->values[primes->len] = i;
+      primes->len++;
     }
   }
-  pg_free(pg);
+  free(flags);
+}
 
-  long result = vec_last(factors);
-  vec_free(factors);
-  return result;
+void primes_free(Primes *primes) {
+  free(primes->values);
+  free(primes);
+}
+
+Primes *primes_new(long target) {
+  Primes *primes = malloc(sizeof(Primes));
+  primes->values = malloc(target * sizeof(long));
+  primes->len = 0;
+  primes_prepare(primes, target);
+  primes->values = realloc(primes->values, primes->len * sizeof(long));
+  return primes;
+}
+
+long solve(long target) {
+  Primes *primes = primes_new(target);
+
+  for (int i = primes->len - 1; i >= 0; i--) {
+    long p = primes->values[i];
+    if (target % p == 0) {
+      primes_free(primes);
+      return p;
+    }
+  }
+
+  primes_free(primes);
+  return 0;
 }
 
 int main(int argc, char **args) {
   long a;
 
-  a = resolve(3);
-  if (a != 3) {
-    printf("expected %ld but got %ld\n", (long)3, a);
-    return 1;
-  }
-
-  a = resolve(4);
+  a = solve(2);
   if (a != 2) {
-    printf("expected %ld but got %ld\n", (long)2, a);
+    printf("expected %ld, but got %ld\n", (long)2, a);
     return 1;
   }
 
-  a = resolve(6);
+  a = solve(3);
   if (a != 3) {
-    printf("expected %ld but got %ld\n", (long)3, a);
+    printf("expected %ld, but got %ld\n", (long)3, a);
     return 1;
   }
 
-  a = resolve(13195);
+  a = solve(13195);
   if (a != 29) {
-    printf("expected %ld but got %ld\n", (long)29, a);
+    printf("expected %ld, but got %ld\n", (long)29, a);
     return 1;
   }
 
-  a = resolve(600851475143);
+  a = solve(600851475143);
   printf("answer = %ld\n", a);
 
   return 0;
